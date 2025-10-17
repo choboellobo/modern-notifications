7b02092325f50bea27f73bcccbf07c781b42aad9
Eduardo Martin <eduardomunozalfonso@MacBook-Air-de-Eduardo.local>
2025-10-17 15:31:28 +0200

    Initial commit

---
+++ example-app/src/js/example.js
@@ -0,0 +1,6 @@
+import { Example } from 'modern-notifications';
+
+window.testEcho = () => {
+    const inputValue = document.getElementById("echoInput").value;
+    Example.echo({ value: inputValue })
+}
